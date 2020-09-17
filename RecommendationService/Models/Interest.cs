e33ae89ed1e50ff5e7cf537dc59785600d689056
Mihail Bondarenco <441044@student.fontys.nl>
2020-09-17 11:44:59 +0200

    feat(*) add scafolded controllers for existing models

--- RecommendationService/Models/Interest.cs
+++ RecommendationService/Models/Interest.cs
@@ -1,11 +1,25 @@
-﻿namespace RecommendationService.Models
+﻿using System.Text.Json.Serialization;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecommendationService.Models
 {
     public class Interest
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public string ImageURI { get; set; }
-        public InterestType Type { get; set; }
+
+        [NotMapped]
+        [JsonIgnore]
+        public InterestType? Type 
+        {
+            get => string.IsNullOrEmpty(TypeString) ? null : Enum.Parse(typeof(InterestType), TypeString) as InterestType?;
+            set => TypeString = value.ToString();
+        }
+
+        [JsonPropertyName("type")]
+        public string TypeString { get; private set; }
     }
 
     public enum InterestType
